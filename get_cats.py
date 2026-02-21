a6b715c6ea496a71eab09c168de6971257dceb3e
Konstantin K <kim9@i.ua>
2026-02-21 12:36:48 +0200

    rework

--- get_cats.py
+++ get_cats.py
@@ -1,25 +1,36 @@
 from pathlib import Path
 
-
 def get_cats_info(path):
+    """
+    Зчитує інформацію про котів із текстового файлу.
+
+    Формат кожного рядка файлу: id,name,age
+
+    Параметри: path (str): шлях до файлу.
+    Повертає: list[dict]: список словників у форматі:{"id": str, "name": str, "age": str}
+    При помилці читання — повертається порожній список.
+    """
     path_file = Path(path)
     cats_info = []
     if not path_file.is_file():
         print(f"Файл {path} не знайдено")
         return []
-
     try:
-        with open(path, "r", encoding = "utf-8") as file:
+        with open(path_file, "r", encoding = "utf-8") as file:
             for line in file:
                 line = line.strip().split(",")
                 if len(line) == 3:
                     cats_info.append({"id":line[0], "name":line[1], "age":line[2]})
-            return cats_info
     except Exception as e:
         print(f"Сталась помилка {e}")
         return []
+    return cats_info
 
 def main():
+    """
+    Точка входу в програму.
+    Викликає функцію зчитування файлу та повертає результат
+    """
     cats_info = get_cats_info("cats_file.txt")
     print(cats_info)
     
